8b592889e085414525f148f0a140d3a074cb10a5
DONGhunee <37871553+DONGhunee@users.noreply.github.com>
2019-03-13 21:40:32 +0900

    se commit

--- kakao/settings.py
+++ kakao/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't$)rx=kuy&4*1lu@$otb0&5x)usg*6xd(#r!2unj9#zbngxj^3'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','t$)rx=kuy&4*1lu@$otb0&5x)usg*6xd(#r!2unj9#zbngxj^3')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
